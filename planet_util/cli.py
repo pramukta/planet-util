f45fd2f3758ce80b05edec1e79baf6d77e252479
Pramukta Kumar <pramukta.kumar@gmail.com>
2018-06-25 18:01:37 -0500

    activate and download work

--- planet_util/cli.py
+++ planet_util/cli.py
@@ -1,5 +1,5 @@
+import os
 import json
-import types
 import datetime as dt
 
 from shapely.geometry import shape, mapping, Polygon
@@ -43,6 +43,7 @@ def build_scene_list(region, date_ranges):
                     coverage_area = coverage.area
             scenes.append(item)
             coverage_area = coverage.area
+            compl
     print("Coverage:", coverage.intersection(region).area / region.area, "Count:", len(scenes))
     return(scenes)
 
@@ -54,20 +55,21 @@ def reduce_scenes(scenes, region):
     recs = sorted([(shape(scene["geometry"]).intersection(region).area, scene) for scene in scenes],
                   key=lambda x: x[0])
     removal_list = []
+    ref_coverage = coverage(scenes, region)
     for idx in range(len(scenes)):
         s = [rec[-1] for i, rec in enumerate(recs) if i != idx and i not in removal_list]
-        if coverage(s, region) == 1.0:
+        if coverage(s, region) == ref_coverage:
             removal_list.append(idx)
     return [rec[-1] for i, rec in enumerate(recs) if i not in removal_list]
-
 
 
 @click.command(help="Create a planetscope mosaic for a specified geojson geometry")
 @click.option("--months", type=int, help="Number of months in the past to use.")
 @click.option("--idx", type=int, help="Index of a single geometry to consider inside the GeoJSON file (starting from 0) [optional]", default=None)
 @click.option("--test", is_flag=True, default=False)
+@click.option("--output", help="Output GeoJSON file")
 @click.argument("geom_file", type=click.Path(exists=True))
-def mosaic(geom_file, months, idx, test):
+def materials(geom_file, months, idx, test, output):
     with open(geom_file) as f:
         # TODO: Error handling
         geojson = json.load(f)
@@ -85,7 +87,43 @@ def mosaic(geom_file, months, idx, test):
     print(len(scenes))
     scenes = reduce_scenes(scenes, region)
     print(len(scenes), coverage(scenes, region))
+    scenes_geojson = {
+        "type": "FeatureCollection",
+        "features": scenes
+    }
+    with open(output, "w") as f:
+        json.dump(scenes_geojson, f)
+
+@click.command(help="Activate scenes in specified file, if they aren't already")
+@click.option("--product", default="visual")
+@click.argument("scenes_file", type=click.Path(exists=True))
+def activate(scenes_file, product):
+    with open(scenes_file) as f:
+        scenes = json.load(f)
+
+    ready_count = 0
+    for idx, scene in enumerate(scenes["features"]):
+        assets = planet.get_assets(scene).get()
+        assert product in assets, "Desired product doesn't exist for specified scene"
+        print(idx, assets[product]["status"])
+        if assets[product]["status"] == "inactive":
+            planet.activate(assets[product])
+        elif (assets[product]["status"]) == "active":
+            ready_count = ready_count + 1
+
+    print("{}/{} images ready to download".format(ready_count, len(scenes["features"])))
+
+@click.command(help="Download scenes in specified file, if they haven't been already")
+@click.option("--product", default="visual")
+@click.option("--path", help="Download images to path", default=os.path.join(os.getcwd(),"planet"), click.Path(exists=True))
+@click.argument("scenes_file", type=click.Path(exists=True))
+def download(scenes_file, product, path):
+    with open(scenes_file) as f:
+        scenes = json.load(f)
+
+    for idx, scene in enumerate(scenes["features"]):
+        asssets = planet.get_assets(scene).get()
 
 
 if __name__ == "__main__":
-    mosaic()
+    activate()
